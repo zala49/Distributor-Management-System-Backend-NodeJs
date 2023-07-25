02e8bb252813e0d086fe6aa90654aaeaef8d795a
sumit patel <84306168+sumit75757@users.noreply.github.com>
2023-07-25 10:08:02 +0530

    2 Update auth.middleware.ts

--- app/middlewares/auth.middleware.ts
+++ app/middlewares/auth.middleware.ts
@@ -15,12 +15,15 @@ export const checkJWT = (req: any, res: any, next: any) => {
 
     next();
   } catch (error: any) {
-    if (error.name === "JsonWebTokenError") {
-      res.status(401).send("Invalid token");
-    } else if (error.name === "TokenExpiredError") {
-      res.status(401).send("Token expired");
-    } else {
-      res.status(401).send("Authentication failed");
+    if(error) {
+      res.status(401).send("Something went wrong!!");
     }
+    // if (error.name === "JsonWebTokenError") {
+    //   res.status(401).send("Invalid token");
+    // } else if (error.name === "TokenExpiredError") {
+    //   res.status(401).send("Token expired");
+    // } else {
+    //   res.status(401).send("Authentication failed");
+    // }
   }
 };
