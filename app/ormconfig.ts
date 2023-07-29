499300572e11faffc9f0aebd9797b4dc5eb73c2b
Hitesh Zala <zalahitesh960@gmail.com>
2023-07-29 12:20:17 +0530

    chnges

--- app/ormconfig.ts
+++ app/ormconfig.ts
@@ -8,11 +8,7 @@ import { CityEntity } from "./model/Tables/city.model";
 import { ProductEntity } from "./model/Tables/product.model";
 import { development } from "../config/environment";
 import { ProductCategoryEntity } from "./model/Tables/productCategory.model";
-import { Newww1690106380230 } from "./migrations/1690106380230-newww";
-import { Hajsbdjbafjks1690111519182 } from "./migrations/1690111519182-hajsbdjbafjks";
-import { Jkbajkdsjkfnajsd1690112970257 } from "./migrations/1690112970257-jkbajkdsjkfnajsd";
-import { Cityerror1690473975475 } from "./migrations/1690473975475-cityerror";
-import { User1690474571956 } from "./migrations/1690474571956-user";
+
 export const AppDataSource: DataSource = new DataSource({
     type:'postgres',
     host: development.host,
@@ -32,6 +28,6 @@ export const AppDataSource: DataSource = new DataSource({
         ProductEntity,
         ProductCategoryEntity
     ],
-    migrations: [User1690474571956],
+    migrations: [],
     synchronize: false
 });
